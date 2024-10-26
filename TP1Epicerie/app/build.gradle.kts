6d69be53f8b6b6f126d47a3be6375c185bdf8ddb
Arizyen <118849721+zarsalan@users.noreply.github.com>
2024-10-25 21:21:25 -0400

    Mise à jour des dépendences

--- TP1Epicerie/app/build.gradle.kts
+++ TP1Epicerie/app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("kotlin-kapt")
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -41,23 +41,20 @@ android {
 }
 
 dependencies {
-
-    val nav_version = "2.7.5"
-    val compose_version = "1.6.0-alpha06"
-    val room = "2.6.1"
+    // Componse
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 
     // Room
-    implementation("androidx.room:room-runtime:$room")
-    implementation("androidx.room:room-ktx:$room")
-    kapt("androidx.room:room-compiler:$room")
-
-    implementation("androidx.navigation:navigation-compose:$nav_version")
-    implementation("androidx.compose.ui:ui:$compose_version")
-    implementation("androidx.compose.material:material:$compose_version")
-    implementation("androidx.compose.ui:ui-tooling-preview:$compose_version")
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.common)
+    implementation(libs.androidx.room.ktx)
 
     //Conversion de list en gson
-    implementation ("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    // Processeur d'annotations pour Room
+    ksp(libs.androidx.room.compiler.ksp)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
