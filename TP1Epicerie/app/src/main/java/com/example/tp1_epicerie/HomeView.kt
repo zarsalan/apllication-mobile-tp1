0956cd157c4f7007731fdb2371230d81a32db6d1
Arizyen <118849721+zarsalan@users.noreply.github.com>
2024-10-26 10:41:33 -0400

    Mise à jour page d'accueil pour afficher les listes et création nouvelles routes

--- TP1Epicerie/app/src/main/java/com/example/tp1_epicerie/HomeView.kt
+++ TP1Epicerie/app/src/main/java/com/example/tp1_epicerie/HomeView.kt
@@ -1,6 +1,8 @@
 package com.example.tp1_epicerie
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
@@ -16,11 +18,11 @@ import androidx.compose.ui.res.colorResource
 import androidx.navigation.NavController
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.tp1_epicerie.data.GroceryList
 
 @Composable
 fun HomeView(navController: NavController, viewModel: GroceryViewModel) {
@@ -29,7 +31,7 @@ fun HomeView(navController: NavController, viewModel: GroceryViewModel) {
             FloatingActionButton(
                 modifier = Modifier.padding(all = 20.dp),
                 contentColor = Color.White,
-                containerColor = colorResource(id = R.color.app_bar_color),
+                containerColor = colorResource(id = R.color.app_bar),
                 onClick = {
                     navController.navigate(Screen.AddScreen.route + "/0L")
                 }
@@ -39,24 +41,66 @@ fun HomeView(navController: NavController, viewModel: GroceryViewModel) {
         }
     ) {
         val groceryList = viewModel.getAllGroceryLists.collectAsState(initial = emptyList())
-        LazyColumn(modifier = Modifier.fillMaxSize().padding(it)){
-            items(groceryList.value){ grocery ->
-                GroceryCard(grocery, navController)
+        LazyColumn(modifier = Modifier
+            .fillMaxSize()
+            .padding(it)) {
+            item {
+                CustomCard(
+                    CardInfo(
+                        "Tous les articles",
+                        "Voir tous les articles",
+                        { navController.navigate(Screen.AllItems.route) },
+                        colorResource(id = R.color.all_items)
+                    )
+                )
+            }
+            item {
+                CustomCard(
+                    CardInfo(
+                        "Favoris",
+                        "Voir les articles favoris",
+                        { navController.navigate(Screen.Favorites.route) },
+                        colorResource(id = R.color.favorite_items)
+                    )
+                )
+            }
+            items(groceryList.value) { grocery ->
+                CustomCard(
+                    CardInfo(
+                        grocery.title,
+                        grocery.description,
+                        { navController.navigate(Screen.GroceryList.route + "/${grocery.id}") },
+                        Color.White
+                    )
+                )
             }
         }
     }
 }
 
+// Carte personnalisée
+data class CardInfo(
+    val title: String,
+    val description: String,
+    val onClick: () -> Unit,
+    val containerColor: Color
+)
+
 @Composable
-fun GroceryCard(grocery: GroceryList, navController: NavController){
+fun CustomCard(cardInfo: CardInfo) {
     Card(
-        modifier = Modifier.padding(8.dp),
-        onClick = {
-            navController.navigate(Screen.GroceryList.route + "/${grocery.id}")
-        }
-    ){
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 4.dp, start = 4.dp, end = 8.dp)
+            .clickable { cardInfo.onClick() },
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp, pressedElevation = 4.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = cardInfo.containerColor,
+            contentColor = Color.Black
+        )
+    ) {
         Text(
-            text = grocery.title,
+            text = cardInfo.title,
             color = Color.Black,
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold,
