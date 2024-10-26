38cf810b4e5abaa3885c1e13b9e1a63c946c6d72
Arizyen <118849721+zarsalan@users.noreply.github.com>
2024-10-26 09:17:39 -0400

    Ajout Screen, Navigation, AppBarView, HomeView et connection de MainActivity

--- TP1Epicerie/app/src/main/java/com/example/tp1_epicerie/HomeView.kt
+++ TP1Epicerie/app/src/main/java/com/example/tp1_epicerie/HomeView.kt
@@ -1,2 +1,9 @@
 package com.example.tp1_epicerie
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun HomeView(navController: NavController, viewModel: GroceryViewModel) {
+
+}
--- TP1Epicerie/app/src/main/java/com/example/tp1_epicerie/Navigation.kt
+++ TP1Epicerie/app/src/main/java/com/example/tp1_epicerie/Navigation.kt
@@ -1,2 +1,28 @@
 package com.example.tp1_epicerie
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+
+@Composable
+fun Navigation(
+    modifier: Modifier = Modifier,
+    viewModel: GroceryViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.HomeScreen.route,
+        modifier = modifier
+    ) {
+        composable(Screen.HomeScreen.route) {
+            HomeView(navController, viewModel)
+        }
+    }
+}
